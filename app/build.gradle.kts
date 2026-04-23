7fed8008a9fb7fcabe97f633fa35639854ddcd07
jedzqer <jedzouquan@gmail.com>
2026-04-23 15:15:17 +0800

    取消isMinifyEnabled和isShrinkResources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             if (releaseSigning != null) {
                 signingConfig = releaseSigning
             } else {
