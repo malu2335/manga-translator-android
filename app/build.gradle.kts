745e6b4230a4686169146b5bc8a798e1a8d1e0eb
jedzqer <jedzouquan@gmail.com>
2026-05-06 12:52:06 +0800

    v3.0.10更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.manga.translate.v3"
         minSdk = 24
         targetSdk = 36
-        versionCode = 50
-        versionName = "3.0.9"
+        versionCode = 51
+        versionName = "3.0.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/manga/translate/VersionInfo.kt
+++ app/src/main/java/com/manga/translate/VersionInfo.kt
@@ -1,6 +1,6 @@
 package com.manga.translate
 
 object VersionInfo {
-    const val VERSION_CODE = 50
-    const val VERSION_NAME = "3.0.9"
+    const val VERSION_CODE = 51
+    const val VERSION_NAME = "3.0.10"
 }
